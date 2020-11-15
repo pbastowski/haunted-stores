3478e85f4f9c51a34b447c21e80126da699f2e46
Paul Bastowski <pbastowski@gmail.com>
2020-11-15 22:44:48 +0100

    Must wrap side effects in useEffect, otherwise we will get a loop at the application startup.

--- src/test2.js
+++ src/test2.js
@@ -1,8 +1,10 @@
-import { html, virtual, json } from './libs'
+import { html, virtual, json, useEffect } from './libs'
 import rootStore from './store/root.js'
 
 export default virtual(title => {
     const [store] = rootStore()
+
+    useEffect(() => store.$set({ text: 'This is TWO' }), [])
 
     return html`
         <h3>This is ${title}</h3>
