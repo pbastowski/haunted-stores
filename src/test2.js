a3bf160d6c02aa64f3e827ab7111d06cd56f0700
Paul Bastowski <pbastowski@gmail.com>
2020-11-16 10:16:29 +0100

    show how to rename a store export

--- src/test2.js
+++ src/test2.js
@@ -1,12 +1,12 @@
 import { html, virtual, json, useEffect } from './libs'
-import { store } from './store/root.js'
+import { store as rootStore } from './store/root.js'
 
 export default virtual(title => {
-    useEffect(() => store.$set({ text: 'Using TEST2' }), [])
+    useEffect(() => rootStore.$set({ text: 'Using TEST2' }), [])
     return html`
         <h3>This is ${title}</h3>
-        <pre>${json(store)}</pre>
-        <button @click=${() => store.add(1)}>+1</button>
+        <pre>${json(rootStore)}</pre>
+        <button @click=${() => rootStore.add(1)}>+1</button>
         ${console.log('@ RENDER test 2')}
     `
 })
